678a283ef1f46c68b1b5b9196b8fad150b97411f
FakerL <56227945+FakerL@users.noreply.github.com>
2019-10-24 21:44:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,20 +12,7 @@ client.on("ready", () =>{
 client.on('message', message => {
 	
 	            if(message.content.startsWith(prefix + 'help')){
-    
-                            let help = new Discord.RichEmbed()
-                            .setColor('RANDOM')
-                            .setThumbnail(`${message.author.avatarURL}`)
-                            .setTitle('**__Voici mes commandes !__**')
-                            .addField("__Bot :__", " `@.support (pour le lien du bot)`")
-                            .addField("__Owner :__", " `@.mp (Et ton message et sa mp all tous ton serveur !)`")
-                            .addField("__Moderation :__", " `@.clear`")
-                            .addField("__Fun :__", " `@.8ball`")
-                            .addField("__Utile :__", "`@.ping , $.serveurs`")
-                            .addField("__Information :__ ","`Les autres commandes seront disponibles dans la version Bêta.`")
-                            .setFooter("By Faker")
-                            .setTimestamp()
-                            message.channel.send(help)
+                         message.channel.send("```──────▄▀▄─────▄▀▄\n─────▄█░░▀▀▀▀▀░░█▄\n─▄▄──█░░░░░░░░░░░█──▄▄\n█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█```")
            }
 	
 		
