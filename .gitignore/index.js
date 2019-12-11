376061420ec93bdb6187c99873f285dbe5ba27ca
FakerL <56227945+FakerL@users.noreply.github.com>
2019-12-11 16:33:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,7 +5,7 @@ let prefix = "%."
 
 client.on("ready", () =>{
     console.log("Je suis prête")
-    client.user.setActivity("Visual Studio Code", {type : 1})
+    client.user.setActivity("Visual Studio Code", {type : 3})
     
 });
 
